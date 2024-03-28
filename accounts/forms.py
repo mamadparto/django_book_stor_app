3c377c43f482907622d7e9036e4b9d015f3ad8e7
mohammad partonia linux <partonia.m@gmail.com>
2024-03-28 14:37:51 +0330

    writing pages code

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,9 +6,9 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age', 'first_name')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age', 'first_name')
