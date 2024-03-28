cb126f74e7582cec611328e67458ea460d1db0dd
mohammad partonia linux <partonia.m@gmail.com>
2024-03-28 11:55:50 +0330

    writing logout code

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,4 @@ from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True) #null for databse  ///  blank for form
-    
-  
+    age = models.PositiveIntegerField(null=True, blank=True)  # null for databse  ///  blank for form
