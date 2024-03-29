c232be066e30ec35166888f0433d81d2be8a6458
mohammad partonia linux <partonia.m@gmail.com>
2024-03-29 15:37:32 +0330

    writing home page test code

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
 ]
